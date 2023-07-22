8c04bc56502eb68d2fb1b014ed2ccb760b3b64a6
allup <allup@MYPC-L>
2023-07-21 22:31:38 -0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Speech.Recognition;
+using System.Windows;
+using Speech2Text;
+
+namespace Speech2Text
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SpeechRecognitionEngine speechRecognitionEngine;
+        private SpeechToTextConverter speechConverter;
+        private bool isListening = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            speechConverter = new SpeechToTextConverter();
+            speechConverter.TextRecognized += SpeechConverter_TextRecognized;
+            ClearButton.Click += ClearButton_Click;
+        }
+        private void SpeechConverter_TextRecognized(object sender, string recognizedText)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                textBox.Text += recognizedText + " ";
+                // Assuming you have a method that converts the recognizedText to the desired format.
+                string convertedText = ConvertRecognizedText(recognizedText);
+                convertedTextBox.Text = convertedText;
+            });
+        }
+        private string ConvertRecognizedText(string recognizedText)
+        {
+            // Your logic to convert the recognized text to the desired format goes here.
+            // For now, let's simply return the recognized text as it is.
+            return recognizedText;
+        }
+        private void StartStopButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!speechConverter.IsListening)
+            {
+                // Start recognition
+                speechConverter.StartRecognition();
+                StartStopButton.Content = "Stop Recognition";
+            }
+            else
+            {
+                // Stop recognition
+                speechConverter.StopRecognition();
+                StartStopButton.Content = "Start Recognition";
+            }
+        }
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Clear();
+            convertedTextBox.Clear();
+        }
+    }
+}
---
+++ SpeechToTextConverter.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Diagnostics;
+using System.Speech.Recognition;
+
+namespace Speech2Text
+{
+    public class SpeechToTextConverter
+    {
+        private SpeechRecognitionEngine speechRecognitionEngine;
+        private bool isInitialized = false;
+        private bool isRecognizing = false;
+
+        public event EventHandler<string> TextRecognized;
+
+        public bool IsListening { get; private set; }
+
+        public SpeechToTextConverter()
+        {
+            InitializeSpeechRecognitionEngine();
+        }
+
+        private void InitializeSpeechRecognitionEngine()
+        {
+            speechRecognitionEngine = new SpeechRecognitionEngine();
+            speechRecognitionEngine.SetInputToDefaultAudioDevice();
+
+            // Load a grammar (we're using DictationGrammar for this example)
+            var dictationGrammar = new DictationGrammar();
+            speechRecognitionEngine.LoadGrammar(dictationGrammar);
+
+            speechRecognitionEngine.SpeechRecognized += SpeechRecognitionEngine_SpeechRecognized;
+            if (!IsListening)
+            {
+                speechRecognitionEngine.RecognizeAsync(RecognizeMode.Multiple);
+                IsListening = true;
+            }
+            IsListening = false;
+        }
+
+
+        private void SpeechRecognitionEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            if (e.Result != null)
+            {
+                Debug.WriteLine("Recognized: " + e.Result.Text + ", Confidence: " + e.Result.Confidence);
+                if (e.Result.Confidence > 0)
+                {
+                    string recognizedText = e.Result.Text;
+                    OnTextRecognized(recognizedText);
+                }
+            }
+            isRecognizing = false;
+        }
+
+
+        protected virtual void OnTextRecognized(string recognizedText)
+        {
+            TextRecognized?.Invoke(this, recognizedText);
+        }
+
+        public void StartRecognition()
+        {
+            if (!isInitialized)
+            {
+                speechRecognitionEngine.RecognizeAsync(RecognizeMode.Multiple);
+                isInitialized = true;
+            }
+
+            if (!IsListening && !isRecognizing)
+            {
+                // Set the flag to indicate that recognition is in progress
+                isRecognizing = true;
+                speechRecognitionEngine.RecognizeAsync();
+                IsListening = true;
+            }
+        }
+
+        public void StopRecognition()
+        {
+            if (IsListening)
+            {
+                speechRecognitionEngine.RecognizeAsyncCancel();
+                IsListening = false;
+            }
+        }
+    }
+}
